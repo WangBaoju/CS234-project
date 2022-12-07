b27557a9f3a91f19a14c85b90ef23395d12c0d29
Baoju Wang <baojuwang@ucr-secure-04-fs-10-12-147-22.wnet.ucr.edu>
2022-12-07 14:19:28 -0800

    Baoju

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-import time
+from suffixTree import *
+import string
+import random
+#import suffixtree as st
+
+# initializing size of string
+N = 100000
+
+ #using random.choices()
+# generating random strings
+#input = ''.join(random.choices(string.ascii_uppercase +string.digits, k=N))
+
+# print result#
+#print("The generated random string : " + str(res))
+
+
+input="abaababaabaababaababa$"
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+suffix_set=set()
+for i in range(len(input)):
+    suffix_set.add(input[i:len(input)])
+print(suffix_set)
+print(len(suffix_set))
+
+
+
+suffixtree=Suffixtree(input)
+suffixtree.build_suffixtree()
+suffixlist=suffixtree.print_dfs()
+print(len(suffixlist))
+if (set(suffixlist)==suffix_set):
+    print("test ok!")
+
+
+
+
