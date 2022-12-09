15b4f855c8e7f29b0bc3c8176760ca4b8e3e9416
Baoju Wang <baojuwang@ucr-secure-04-fs-10-12-147-22.wnet.ucr.edu>
2022-12-09 15:41:40 -0800

    BaojuWang

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-from suffixTree import *
-import string
+from mySuffixTree import *
+import suffixTree as st
+import time
 import random
-#import suffixtree as st
-
-# initializing size of string
-N = 100000
-
- #using random.choices()
-# generating random strings
-#input = ''.join(random.choices(string.ascii_uppercase +string.digits, k=N))
-
-# print result#
-#print("The generated random string : " + str(res))
+import string
 
 
-input="abaababaabaababaababa$"
+#-------------The smaller test----------------------------
+#input="banana$"
+#input = "Mississippi$"
 
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
+
+#-----------The bigger test-------------------------------
+
+# initialize the size of the input string
+size = 50000
+
+#using random.choices() generating random strings
+input = ''.join(random.choices(string.ascii_lowercase, k= size))
+input = input + "$"
+
+
 suffix_set=set()
 for i in range(len(input)):
     suffix_set.add(input[i:len(input)])
-print(suffix_set)
 print(len(suffix_set))
 
+print(f"The input size is: {len(input)}")
+startTime = time.perf_counter()
+
+suffixtree1=mySuffixTree(input)
+suffixtree1.build_suffixtree()
+
+endTime = time.perf_counter()
+print(f"Build the suffix using my implementation in {endTime - startTime:0.4f} seconds")
+
+startTime = time.perf_counter()
+suffixtree2= st.Suffixtree(input)
+suffixtree2.build_suffixtree()
 
 
-suffixtree=Suffixtree(input)
-suffixtree.build_suffixtree()
-suffixlist=suffixtree.print_dfs()
-print(len(suffixlist))
-if (set(suffixlist)==suffix_set):
-    print("test ok!")
+endTime = time.perf_counter()
+print(f"Build the suffix using the suffix-tree library in {endTime - startTime:0.4f} seconds")
 
+suffixlist1=suffixtree1.print_dfs()
+print(len(suffixlist1))
+#print(suffixlist1)
 
-
-
+if (set(suffixlist1)==suffix_set):
+    print("The suffixes tree in my implementation contains all the suffixes.")
